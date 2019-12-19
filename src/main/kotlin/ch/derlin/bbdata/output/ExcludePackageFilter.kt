6ceeac36da7cb9a13a1100adb3b914159a221a5b
Lucy Linder <lucy.derlin@gmail.com>
2019-12-19 19:02:22 +0100

    improve package filter
    - allow array

--- src/main/kotlin/ch/derlin/bbdata/output/ExcludePackageFilter.kt
+++ src/main/kotlin/ch/derlin/bbdata/output/ExcludePackageFilter.kt
@@ -1,10 +1,15 @@
 package ch.derlin.bbdata.output
 
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.context.EnvironmentAware
+import org.springframework.context.annotation.Configuration
 import org.springframework.core.env.Environment
 import org.springframework.core.type.classreading.MetadataReader
 import org.springframework.core.type.classreading.MetadataReaderFactory
 import org.springframework.core.type.filter.TypeFilter
+import org.springframework.stereotype.Component
 import java.io.IOException
 
 /**
@@ -21,19 +26,23 @@ import java.io.IOException
  * Derived from [this StackOverflow answer](https://stackoverflow.com/a/54381921)
  */
 class ExcludePackageFilter : TypeFilter, EnvironmentAware {
-    private lateinit var env: Environment
+
+    companion object {
+        private const val PROP_NAME = "component-scan.exclude"
+    }
+
+    private var excludedPackages: Array<String>? = null
 
     @Throws(IOException::class)
     override fun match(metadataReader: MetadataReader, metadataReaderFactory: MetadataReaderFactory): Boolean {
-        var match = false
-        env.getProperty("componentscan.exclude.package")?.let { excludedPackage ->
-            match = metadataReader.classMetadata.className.startsWith(excludedPackage)
+        if (excludedPackages != null) {
+            val cls = metadataReader.classMetadata.className
+            return excludedPackages?.any { cls.startsWith(it) } ?: false
         }
-        return match
+        return false
     }
 
     override fun setEnvironment(environment: Environment) {
-        env = environment
+        excludedPackages = environment.getProperty(PROP_NAME, Array<String>::class.java)
     }
-
 }
