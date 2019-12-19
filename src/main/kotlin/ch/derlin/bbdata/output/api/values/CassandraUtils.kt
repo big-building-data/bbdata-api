036be4aa3aaa98e28255957f2c94676e89c6376c
Lucy Linder <lucy.derlin@gmail.com>
2019-12-19 12:30:29 +0100

    move cassandrautils

---
+++ src/main/kotlin/ch/derlin/bbdata/output/api/values/CassandraUtils.kt
@@ -0,0 +1,28 @@
+package ch.derlin.bbdata.output.api.values
+
+import org.joda.time.YearMonth
+
+/**
+ * date: 19.12.19
+ * @author Lucy Linder <lucy.derlin@gmail.com>
+ */
+
+
+object CassandraUtils {
+    val YM_FORMAT = org.joda.time.format.DateTimeFormat.forPattern("yyyy-MM")
+
+    fun xMonthsFrom(d1: YearMonth, x: Int): List<String> {
+        val ym = YearMonth(d1)
+        return monthsBetween(ym.minusMonths(x), ym)
+    }
+
+    fun monthsBetween(d1: YearMonth, d2: YearMonth? = null): List<String> {
+        val months = mutableListOf<String>()
+        var d2 = d2 ?: YearMonth.now()
+        while (d1.compareTo(d2) <= 0) {
+            months.add(YM_FORMAT.print(d2))
+            d2 = d2.minusMonths(1)
+        }
+        return months
+    }
+}
--- src/main/kotlin/ch/derlin/bbdata/output/api/values/RawValue.kt
+++ src/main/kotlin/ch/derlin/bbdata/output/api/values/RawValue.kt
@@ -61,25 +61,6 @@ data class RawValue(
 
 }
 
-object CassandraUtils {
-    val YM_FORMAT = org.joda.time.format.DateTimeFormat.forPattern("yyyy-MM")
-
-    fun xMonthsFrom(d1: YearMonth, x: Int): List<String> {
-        val ym = YearMonth(d1)
-        return monthsBetween(ym.minusMonths(x), ym)
-    }
-
-    fun monthsBetween(d1: YearMonth, d2: YearMonth? = null): List<String> {
-        val months = mutableListOf<String>()
-        var d2 = d2 ?: YearMonth.now()
-        while (d1.compareTo(d2) <= 0) {
-            months.add(YM_FORMAT.print(d2))
-            d2 = d2.minusMonths(1)
-        }
-        return months
-    }
-}
-
 
 @Repository
 interface RawValueRepository : CassandraRepository<RawValue, RawValuePK> {
